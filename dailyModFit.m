2650ac418a6fcb50c6a3d44784c2d5bbeb06f3cc
Carson <blinnc@uw.edu>
2018-06-27 19:16:50 -0700

    Complete dailyModFit file

--- dailyModFit.m
+++ dailyModFit.m
@@ -1,4 +1,11 @@
-function dailyModFit(A,B)
-	omegaEarthHr = 2*pi*
+function [betaModA,betaModB] = dailyModFit(A,B)
+	
+  omegaEarth = 2*pi*(1/86164.0916);
+  omegaEarthHr = omegaEarth*3600; %(3600 s / 1 hr)
 	designMatrix = [ones(columns(A)),sin(omegaEarthHr.*((1:columns(A))')),cos(omegaEarthHr.*((1:columns(A))'))];
+  [bA,sA,rA,errA,covA] = ols2(A(2:end,:)',designMatrix);
+  [bB,sB,rB,errB,covB] = ols2(B(2:end,:)',designMatrix);
+  betaModA = [A(:,1), bA'];
+  betaModB = [B(:,1), bB'];
+  
 endfunction
