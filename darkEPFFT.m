d91ea0b04026f5522acf96297d01d7d278c667ca
Carson Blinn <blinnc@uw.edu>
2019-06-04 17:01:44 -0700

    Added darkEPFTT and fftPhaseCorrection testers, need fixes

--- darkEPFFT.m
+++ darkEPFFT.m
@@ -4,8 +4,8 @@ function [A,B,t] = darkEPFFT(data)
 	hourLength = rows(data{1,1});
 
 	%Collects the array into a form that fft can operate on
-	preCollectArray = cell2mat(divHours(:,1));
-	collectArray = reshape(preCollectArray,hourLength,rows(divHours));
+	preCollectArray = cell2mat(data(:,1));
+	collectArray = reshape(preCollectArray(:,2),hourLength,rows(data));
 
 	%Calculates fft for each chunk simultaneously multiplied by 2 for one sided power spectrum
 	compOut = (2/rows(collectArray)).*fft(collectArray);
@@ -14,40 +14,34 @@ function [A,B,t] = darkEPFFT(data)
 	%Since array is symmetric about the nyquist frequency, half the matrix can be removed
 	compOut = compOut(1:((rows(compOut)/2) + 1),:);
 
-	%Corrects phase for fft starting at different times
-	colCorrect = exp((2*pi*i*(hourLength)).*fSeries);
-	corArray = zeros(rows(fSeries),columns(compOut));
-	for count = 1:columns(compOut)
-		corArray(:,count) = colCorrect.^(count-1);
-	endfor
-	compOut = compOut.*corArray;	
-
 	%1 and end values should not be multiplied by 2
 	compOut(1,:) = compOut(1,:)./2;
 	compOut(end,:) = compOut(end,:)./2;
+
+	%Corrects phase values to reflect hours they started at
+	phaseCorrected = fftPhaseCorrection(compOut,fSeries,hourLength);
 	
 	%Prepares output arrays
-	A = [fSeries,real(compOut)]; %Cosine components of FFT (columns correspond to hours)
-	B = [fSeries,imag(compOut)]; %Sine components of FFT (columns correspond to hours)
+	A = [fSeries,real(phaseCorrected)]; %Cosine components of FFT (columns correspond to hours)
+	B = [fSeries,imag(phaseCorrected)]; %Sine components of FFT (columns correspond to hours)
 	t = cell2mat(data(:,2)); %Labels hour counter for each row
 endfunction
 
-##%!test
-##%! t = (1:4096)';
-##%! A = 1; omega = 2*pi*(1/100);
-##%! data = [t,A.*sin(omega.*t)];
-##%! divHours = cell(1,2);
-##%! for count = 1:10
-##%! 	divHours{count,1} = data; divHours{count,2} = count;
-##%! endfor
-##%! [cosTerms,sinTerms,t] = darkEPFFT(divHours);
-##%! compOut = (cosTerms(:,2:end) + i*sinTerms(:,2:end));
-##%! compOut(1,:) = 2.*compOut(1,:);
-##%! compOut(end,:) = 2.*compOut(end,:);
-##%! fullCompOut = [compOut;conj(flip(compOut(2:end-1,:)))];
-##%! fullCompOut = (4096/2).*fullCompOut;
-##%! recoveredTime = ifft(fullCompOut);
-##%! for count = 1:10 %Checks that having multiple rows doesn't mess up analysis
-##%! 	assert(real(recoveredTime(:,count)),data(:,2),4*eps);%Checks that recreated time series is the same
-##%! 	assert(fullCompOut(:,count)'*fullCompOut(:,count)/4096,data(:,2)'*data(:,2));%Tests parseval's theorem
-##%! endfor
+%!test
+%! t = (1:4096)';
+%! data = [t,randn(rows(t),1)];
+%! divHours = cell(1,2);
+%! for count = 1:10
+%! 	divHours{count,1} = data; divHours{count,2} = count;
+%! endfor
+%! [cosTerms,sinTerms,t] = darkEPFFT(divHours);
+%! compOut = (cosTerms(:,2:end) + i*sinTerms(:,2:end));
+%! compOut(1,:) = 2.*compOut(1,:);
+%! compOut(end,:) = 2.*compOut(end,:);
+%! fullCompOut = [compOut;conj(flip(compOut(2:end-1,:)))];
+%! fullCompOut = (4096/2).*fullCompOut;
+%! recoveredTime = ifft(fullCompOut);
+%! for count = 1:10 %Checks that having multiple rows doesn't mess up analysis
+%! 	%assert(real(recoveredTime(:,count)),data(:,2),4*eps);%Checks that recreated time series is the same
+%! 	assert(fullCompOut(:,count)'*fullCompOut(:,count)/4096,data(:,2)'*data(:,2));%Tests parseval's theorem
+%! endfor
--- fftPhaseCorrection.m
+++ fftPhaseCorrection.m
@@ -4,20 +4,25 @@ function rtn = fftPhaseCorrection(compOut,freqArray,hourLength)
 	endif
 
 	%Corrects phase for fft starting at different times
-	colCorrect = exp((2*pi*i*(hourLength)).*fSeries);
-	corArray = zeros(rows(fSeries),columns(compOut));
+	colCorrect = exp((2*pi*i*(hourLength)).*freqArray);
+	corArray = zeros(rows(freqArray),columns(compOut));
 	for count = 1:columns(compOut)
 		corArray(:,count) = colCorrect.^(count-1);
-	endfor
-	compOut = compOut.*corArray;	
+	endfor	
 
 	%Returns modified compOut corrected for phase
-	rtn = compOut;
+	rtn = compOut.*corArray;
 endfunction
 
 %!test
+%! numHours = 10;
 %! hourLength = 4096;
 %! freqArray = (0:hourLength/2)'./hourLength;
-%! cosineCheck = ones(hourLength*numHours,1);
+%! cosineCheck = ones(hourLength/2+1,numHours);
 %! sineCheck = i.*cosineCheck;
-%! 
+%! correctedSine=fftPhaseCorrection(sineCheck,freqArray,hourLength);
+%! correctedCosine=fftPhaseCorrection(cosineCheck,freqArray,hourLength);
+%! for count = 1:columns(correctedSine)
+%! 	assert(real(correctedSine)(:,count),sin(2*pi*hourLength.*freqArray*count));
+%! 	assert(real(correctedCosine)(:,count),cos(2*pi*hourLength.*freqArray*count));
+%! endfor
